c05cc6673f96fee32a3e8f027b90e49af4597982
Wilson Sum <wssum@myseneca.ca>
2024-02-14 17:42:55 -0500

    Schedule form complete

--- scheduleMaker/scheduleFuncs.js
+++ scheduleMaker/scheduleFuncs.js
@@ -1,234 +1,354 @@
 const employees = [
-    {
-      name: "Athrun Zala",
+  {
+    name: "Athrun Zala",
+    daysAvailable: [
+      {name: "Sunday", from: 9, to: 14},
+      {name: "Monday", from: 9, to: 14}, 
+      {name: "Tuesday", from: 10, to: 22}, 
+      {name: "Wednesday", from: 9, to: 14}
+    ]
+  },
+  {
+    name: "Greg Capall",
+    daysAvailable: [
+      {name: "Thursday", from: 15, to: 23}, 
+      {name: "Friday", from: 15, to: 22}, 
+      {name: "Saturday", from: 15, to: 24}
+    ]
+  },
+  {
+    name: "Homer Simpson", 
+    daysAvailable: [
+      {name: "Sunday", from: 9, to: 22}, 
+      {name: "Monday", from: 9, to: 22}, 
+      {name: "Tuesday", from: 9, to: 22}, 
+      {name: "Wednesday", from: 9, to: 22}, 
+      {name: "Thursday", from: 9, to: 23}, 
+      {name: "Friday", from: 9, to: 24}, 
+      {name: "Saturday", from: 8, to: 24}
+    ]
+  },
+  {
+    name: "Will Sum",
+    daysAvailable: [
+      {name: "Monday", from: 15, to: 22}, 
+      {name: "Tuesday", from: 15, to: 22}, 
+      {name: "Wednesday", from: 15, to: 22}, 
+      {name: "Sunday", from: 15, to: 22}
+    ]
+  },
+  {
+    name: "Avery Man", 
+    daysAvailable: [
+      {name: "Thursday", from: 9, to: 14}, 
+      {name: "Friday", from: 9, to: 14}, 
+      {name: "Saturday", from: 8, to: 14}, 
+      {name: "Sunday", from: 9, to: 14}
+    ]
+  },
+  {
+    name: "Marge Simpson",
+    daysAvailable: [
+      {name: "Monday", from: 9, to: 22}, 
+      {name: "Wednesday", from: 9, to: 22},
+      {name: "Thursday", from: 9, to: 14}, 
+      {name: "Friday", from: 9, to: 24}
+    ]
+  },
+  {
+    name: "Tony Lau",
+    daysAvailable: [
+      {name: "Tuesday", from: 9, to: 22},
+      {name: "Friday", from: 15, to: 24}, 
+      {name: "Thursday", from: 9, to: 23}, 
+      {name: "Saturday", from: 8, to: 24}
+    ]
+  },
+  {
+      name: "Ray Gibson",
       daysAvailable: [
-        {name: "Sunday", from: 7, to: 12},
-        {name: "Monday", from: 7, to: 12}, 
-        {name: "Tuesday", from: 10, to: 22}, 
-        {name: "Wednesday", from: 7, to: 12}
+       {name: "Saturday", from: 8, to: 14},
+        {name: "Friday", from: 15, to: 24}, 
+        {name: "Thursday", from: 9, to: 23}
       ]
     },
     {
-      name: "Greg Capall",
+      name: "Billy Jeans",
       daysAvailable: [
-        {name: "Thursday", from: 15, to: 22}, 
-        {name: "Friday", from: 15, to: 22}, 
-        {name: "Saturday", from: 15, to: 22}
+       {name: "Saturday", from: 15, to: 24},
+        {name: "Sunday", from: 9, to: 22}
       ]
-    },
-    {
-      name: "Homer Simpson", 
-      daysAvailable: [
-        {name: "Sunday", from: 7, to: 22}, 
-        {name: "Monday", from: 7, to: 22}, 
-        {name: "Tuesday", from: 7, to: 22}, 
-        {name: "Wednesday", from: 7, to: 22}, 
-        {name: "Thursday", from: 7, to: 22}, 
-        {name: "Friday", from: 7, to: 22}, 
-        {name: "Saturday", from: 7, to: 22}
-      ]
-    },
-    {
-      name: "Will Sum",
-      daysAvailable: [
-        {name: "Monday", from: 15, to: 22}, 
-        {name: "Tuesday", from: 15, to: 22}, 
-        {name: "Wednesday", from: 15, to: 22}, 
-        {name: "Sunday", from: 15, to: 22}
-      ]
-    },
-    {
-      name: "Avery Man", 
-      daysAvailable: [
-        {name: "Thursday", from: 7, to: 12}, 
-        {name: "Friday", from: 7, to: 12}, 
-        {name: "Saturday", from: 7, to: 12}, 
-        {name: "Sunday", from: 7, to: 12}
-      ]
-    },
-    {
-      name: "Marge Simpson",
-      daysAvailable: [
-        {name: "Monday", from: 7, to: 22}, 
-        {name: "Wednesday", from: 7, to: 22},
-        {name: "Thursday", from: 7, to: 12}, 
-        {name: "Friday", from: 7, to: 22}
-      ]
-    },
-    {
-      name: "Tony Lau",
-      daysAvailable: [
-        {name: "Tuesday", from: 7, to: 22},
-        {name: "Friday", from: 15, to: 22}, 
-        {name: "Thursday", from: 7, to: 22}, 
-        {name: "Saturday", from: 7, to: 22}
-      ]
-    },
-    {
-        name: "Ray Gibson",
-        daysAvailable: [
-         {name: "Saturday", from: 7, to: 12},
-          {name: "Friday", from: 15, to: 22}, 
-          {name: "Thursday", from: 7, to: 22}
-        ]
-      }
-  ];
-  
-  let schedule = [
-    {
-      day: "Monday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 1,
-      closersRequired: 1,
-      allAroundsRequired: 1,
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Tuesday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 1,
-      closersRequired: 1,
-      allAroundsRequired: 1,
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Wednesday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 1,
-      closersRequired: 1,
-      allAroundsRequired: 2, 
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Thursday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 1,
-      closersRequired: 2,
-      allAroundsRequired: 2, 
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Friday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 1,
-      closersRequired: 2,
-      allAroundsRequired: 2, 
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Saturday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 2,
-      closersRequired: 1,
-      allAroundsRequired: 2, 
-      open: 7,
-      close: 22
-    },
-    {
-      day: "Sunday",
-      opener: [],
-      closer: [],
-      allAround: [],
-      openersRequired: 2,
-      closersRequired: 1,
-      allAroundsRequired: 1, 
-      open: 7,
-      close: 22
     }
-  ];
-  function whichShift(worker, workDay) {
-    let shift = null;
-    worker.daysAvailable.forEach((shiftDay) => {
-        if (shiftDay.name.toLowerCase() === workDay.day.toLowerCase()) {
-            if (shiftDay.from == workDay.open && shiftDay.to < workDay.close) {
-                if(workDay.opener.length < workDay.openersRequired)
-                {
-                    shift = "O";
-                }
-            } else if (shiftDay.from > workDay.open && shiftDay.to == workDay.close) {
-                if(workDay.closer.length < workDay.closersRequired)
-                {
-                    shift = "C";
-                }
+];
 
-            } else if (shiftDay.from == workDay.open && shiftDay.to == workDay.close) {
-                if(workDay.allAround.length < workDay.allAroundsRequired)
-                {
-                    shift = "A";
-                }
-                else if(workDay.closer.length < workDay.closersRequired)
-                {
-                    shift = "C";
-                }
-                else if(workDay.opener.length < workDay.openersRequired)
-                {
-                    shift = "O";
-                }
-            }
-        }
-    });
-    return shift;
+let schedule = [
+  {
+    day: "Monday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 1,
+    allAroundsRequired: 1,
+    open: 9,
+    close: 22
+  },
+  {
+    day: "Tuesday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 1,
+    allAroundsRequired: 1,
+    open: 9,
+    close: 22
+  },
+  {
+    day: "Wednesday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 1,
+    allAroundsRequired: 2, 
+    open: 9,
+    close: 22
+  },
+  {
+    day: "Thursday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 2,
+    allAroundsRequired: 2, 
+    open: 9,
+    close: 23
+  },
+  {
+    day: "Friday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 2,
+    allAroundsRequired: 2, 
+    open: 9,
+    close: 24
+  },
+  {
+    day: "Saturday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 2,
+    closersRequired: 2,
+    allAroundsRequired: 2, 
+    open: 8,
+    close: 24
+  },
+  {
+    day: "Sunday",
+    opener: [],
+    closer: [],
+    allAround: [],
+    openersRequired: 1,
+    closersRequired: 1,
+    allAroundsRequired: 2, 
+    open: 9,
+    close: 22
+  }
+];
+function whichShift(worker, workDay) {
+  let shift = null;
+  worker.daysAvailable.forEach((shiftDay) => {
+      if (shiftDay.name.toLowerCase() === workDay.day.toLowerCase()) {
+          if (shiftDay.from == workDay.open && shiftDay.to < workDay.close) {
+              if(workDay.opener.length < workDay.openersRequired)
+              {
+                  shift = "O";
+              }
+          } else if (shiftDay.from > workDay.open && shiftDay.to == workDay.close) {
+              if(workDay.closer.length < workDay.closersRequired)
+              {
+                  shift = "C";
+              }
+
+          } else if (shiftDay.from == workDay.open && shiftDay.to == workDay.close) {
+              if(workDay.allAround.length < workDay.allAroundsRequired)
+              {
+                  shift = "A";
+              }
+              else if(workDay.closer.length < workDay.closersRequired)
+              {
+                  shift = "C";
+              }
+              else if(workDay.opener.length < workDay.openersRequired)
+              {
+                  shift = "O";
+              }
+          }
+      }
+  });
+  return shift;
 }
 
 //make days have anouther attribute for each for number of openenrs, closers and allaround
 function makeSchedule(workers) {
-    schedule.forEach(function(date) {
-        workers.forEach((worker) => {
-            const isAvailable = worker.daysAvailable.some(workerDay => workerDay.name.toLowerCase() === date.day.toLowerCase());
-            
-            if (isAvailable) {
-                if ((whichShift(worker,date)== "A") && date.allAround.length < date.allAroundsRequired) {
-                    if(!date.allAround.some(workMan=>workMan.name == worker.name))
-                    {
-                        date.allAround.push(worker.name);
-                    }
-                } else if ((whichShift(worker,date)== "O") && (date.opener.length < date.openersRequired)) {
-                    if(!date.opener.some(workMan=>workMan.name == worker.name))
-                    {
-                        date.opener.push(worker.name);  
-                    }
-                } else if ((whichShift(worker,date)== "C") && (date.closer.length < date.closersRequired)) {
-                    if(!date.closer.some(workMan=>workMan.name == worker.name))
-                    {
-                        date.closer.push(worker.name);
-                    }
-                }
-                else if((date.allAround.length >=1)&&(date.opener.length === 0)&&(whichShift(worker,date)== "O"))
-                {
-                    if(!date.opener.some(workMan=>workMan.name == worker.name))
-                    {
-                        date.opener.push(worker.name);  
-                    }
-                }
-                else if((date.allAround.length >=1)&&(date.opener.length === 0)&&(whichShift(worker,date)== "C"))
-                {
-                    if(!date.closer.some(workMan=>workMan.name == worker.name))
-                    {
-                        date.closer.push(worker.name);
-                    }
-                }
-            }
-        });
-    });
+  schedule.forEach(function(date) {
+      workers.forEach((worker) => {
+          const isAvailable = worker.daysAvailable.some(workerDay => workerDay.name.toLowerCase() === date.day.toLowerCase());
+          
+          if (isAvailable) {
+              if ((whichShift(worker,date)== "A") && date.allAround.length < date.allAroundsRequired) {
+                  if(!date.allAround.some(workMan=>workMan.name == worker.name))
+                  {
+                      date.allAround.push(worker.name);
+                  }
+              } else if ((whichShift(worker,date)== "O") && (date.opener.length < date.openersRequired)) {
+                  if(!date.opener.some(workMan=>workMan.name == worker.name))
+                  {
+                      date.opener.push(worker.name);  
+                  }
+              } else if ((whichShift(worker,date)== "C") && (date.closer.length < date.closersRequired)) {
+                  if(!date.closer.some(workMan=>workMan.name == worker.name))
+                  {
+                      date.closer.push(worker.name);
+                  }
+              }
+              else if((date.allAround.length >=1)&&(date.opener.length === 0)&&(whichShift(worker,date)== "O"))
+              {
+                  if(!date.opener.some(workMan=>workMan.name == worker.name))
+                  {
+                      date.opener.push(worker.name);  
+                  }
+              }
+              else if((date.allAround.length >=1)&&(date.opener.length === 0)&&(whichShift(worker,date)== "C"))
+              {
+                  if(!date.closer.some(workMan=>workMan.name == worker.name))
+                  {
+                      date.closer.push(worker.name);
+                  }
+              }
+          }
+      });
+  });
+}
+
+function scheduleReq(daysOfWork)
+{
+  return new Promise((resolve,reject)=>
+  {
+    try{
+      let workDays = []; // Assuming this array is meant to store all day objects
+
+      let sun = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Sunday",
+          allAroundsRequired: daysOfWork.sunAllAroundsRequired,
+          openersRequired: daysOfWork.sunOpenersRequired,
+          closersRequired: daysOfWork.sunClosersRequired,
+          open: daysOfWork.sunOpen,
+          close: daysOfWork.sunClose,
+      };
+      workDays.push(sun);
+      
+      let mon = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Monday",
+          allAroundsRequired: daysOfWork.monAllAroundsRequired,
+          openersRequired: daysOfWork.monOpenersRequired,
+          closersRequired: daysOfWork.monClosersRequired,
+          open: daysOfWork.monOpen,
+          close: daysOfWork.monClose,
+      };
+      workDays.push(mon);
+      
+      let tues = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Tuesday",
+          allAroundsRequired: daysOfWork.tuesAllAroundsRequired,
+          openersRequired: daysOfWork.tuesOpenersRequired,
+          closersRequired: daysOfWork.tuesClosersRequired,
+          open: daysOfWork.tuesOpen,
+          close: daysOfWork.tuesClose,
+      };
+      workDays.push(tues);
+      
+      let wed = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Wednesday",
+          allAroundsRequired: daysOfWork.wedAllAroundsRequired,
+          openersRequired: daysOfWork.wedOpenersRequired,
+          closersRequired: daysOfWork.wedClosersRequired,
+          open: daysOfWork.wedOpen,
+          close: daysOfWork.wedClose,
+      };
+      workDays.push(wed);
+      
+      let thurs = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Thursday",
+          allAroundsRequired: daysOfWork.thursAllAroundsRequired,
+          openersRequired: daysOfWork.thursOpenersRequired,
+          closersRequired: daysOfWork.thursClosersRequired,
+          open: daysOfWork.thursOpen,
+          close: daysOfWork.thursClose,
+      };
+      workDays.push(thurs);
+      
+      let fri = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Friday",
+          allAroundsRequired: daysOfWork.friAllAroundsRequired,
+          openersRequired: daysOfWork.friOpenersRequired,
+          closersRequired: daysOfWork.friClosersRequired,
+          open: daysOfWork.friOpen,
+          close: daysOfWork.friClose,
+      };
+      workDays.push(fri);
+      
+      let sat = {
+          opener: [],
+          closer: [],
+          allAround: [],
+          day: "Saturday",
+          allAroundsRequired: daysOfWork.satAllAroundsRequired,
+          openersRequired: daysOfWork.satOpenersRequired,
+          closersRequired: daysOfWork.satClosersRequired,
+          open: daysOfWork.satOpen,
+          close: daysOfWork.satClose,
+      };
+      workDays.push(sat);
+      schedule = workDays;         
+      resolve(workDays);
+    }catch(error)
+    {
+       reject(error);
+    }
+  })
+}
+
+function newWorker(employ)
+{
+
 }
 
 makeSchedule(employees);
 
 console.log(schedule);
+
+module.exports ={makeSchedule,schedule,employees,whichShift,scheduleReq,newWorker};
---
+++ scheduleMaker/server.js
@@ -0,0 +1,35 @@
+let express = require("express");
+let app = express();
+let scheduleFuncs = require("./scheduleFuncs");
+let port = process.env.port || 8080;
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+res.render("home");
+});
+
+app.get("/editSchedule",(req,res)=>{
+        res.render("scheduleForm",);
+    });
+
+app.post("/editSchedule",(req,res)=>{
+    scheduleFuncs.scheduleReq(req.body).then((data)=>{
+        res.render("scheduleForm",);
+    }).catch(err=>console.log(err));
+  
+    });
+
+    app.get("/newEmployee",(req,res)=>{
+        res.render("newEmployee",);
+    });
+
+app.post("/newEmployee",(req,res)=>{
+    scheduleFuncs.newWorker(req.body).then((data)=>{
+        res.render("newEmployee",);
+    }).catch(err=>console.log(err));
+  
+    });
+
+app.listen(port,()=>(console.log(`Standing by on port ${port}`)));
