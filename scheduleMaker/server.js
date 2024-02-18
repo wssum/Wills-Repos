1f9de2f4bfa881b8f25a03bcc1e8e530e1a3809b
Wilson Sum <wssum@myseneca.ca>
2024-02-17 20:15:47 -0500

    Functions all working just need to worry about the css

--- scheduleMaker/server.js
+++ scheduleMaker/server.js
@@ -1,10 +1,10 @@
 let express = require("express");
 let app = express();
 let scheduleFuncs = require("./scheduleFuncs");
-let port = process.env.port || 8081;
+let port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.get("/",(req,res)=>{
 res.render("home");
